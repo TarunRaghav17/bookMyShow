2cb56a49e266ae9e56a98e7e0d63275389a5e215
Inzamam-khan <khanloveekhan>
2025-08-29 00:21:56 +0530

    working on filter

--- src/app/modules/explore/movies/movies-landingpage/movies-landingpage.component.ts
+++ src/app/modules/explore/movies/movies-landingpage/movies-landingpage.component.ts
@@ -10,16 +10,125 @@ import { CommonService } from '../../../../services/common.service';
 })
 export class MovieLandingPageComponent {
   dummyMoviesdata: any[] = [];
-  selectedFilters: any[] = []
-  topFiltersArray: any[] = ['Hindi', 'English', 'Gujrati', 'Marathi', 'Malayalam', 'Punjabi', 'Telugu'];
+  filters: any[] = [
+    {
+      type: "Language",
+      data: [
+        { text: "Hindi", index: 0, selected: false },
+        { text: "English", index: 1, selected: false },
+        { text: "Gujrati", index: 2, selected: false },
+        { text: "Marathi", index: 3, selected: false },
+        { text: "Malayalam", index: 4, selected: false },
+        { text: "Punjabi", index: 5, selected: false },
+        { text: "Telugu", index: 6, selected: false }
+      ]
+    },
+    {
+      type: "Genres",
+      data: [
+        { text: "Drama", index: 0, selected: false },
+        { text: "Action", index: 1, selected: false },
+        { text: "Comedy", index: 2, selected: false },
+        { text: "Thriller", index: 3, selected: false }
+      ]
+    },
+    {
+      type: "Formats",
+      data: [
+        { text: "2D", index: 0, selected: false },
+        { text: "3D", index: 1, selected: false },
+        { text: "4Dx", index: 2, selected: false },
+        { text: "IMAX2d", index: 3, selected: false }
+      ]
+    }
+  ];
+
+
+  selectedFilters: any[] = [
+    {
+      type: "Language",
+      data: []
+    },
+    // {
+    //    type: "Genres",
+    //   data: []
+    // },
+    //  {
+    //   type: "Formats",
+    //   data: []
+    // }
+  ]
+  topFiltersArray: any[] = [
+    { text: "Hindi", index: 0, },
+    { text: "English", index: 1 },
+    { text: "Gujrati", index: 2 },
+    { text: "Marathi", index: 3 },
+    { text: "Malayalam", index: 4 },
+    { text: "Punjabi", index: 5 },
+    { text: "Telugu", index: 6 }
+  ]
   originalMovies = movies;
   constructor(public commonService: CommonService) {
     this.dummyMoviesdata = movies;
   }
 
   handleEventFilter(filter: any) {
-    this.selectedFilters.push(filter)
-  }
+  //   console.log(filter)
+  // this.selectedFilters.map((item:any)=>{
+  //   if(item.type==filter.type){
+  //     item.data.push(filter.filterName )
+  //   }
 
+  // })
+
+
+  // this.selectedFilters.sort((a,b)=>
+  //   a.index-b.index
+  // )
+
+
+  // this.topFiltersArray.push(filter.filterName)
+  // this.topFiltersArray.sort((a,b)=>
+  //   a.index-b.index
+  // )
+
+  // console.log(this.topFiltersArray)
+
+
+  // }
+
+
+
+// make selected filter appear background red
+  this.filters.filter((item:any)=>{
+    if(item.type== filter.type){
+      item.data.filter((i:any)=>{
+        if(i.text==filter.filterName.text){
+          i.selected=!i.selected
+        }
+
+      })
+    }
+    
+  }
+)
+
+
+let filterType=this.selectedFilters.filter((item:any)=>{
+  item.type==filter.type
+})
+// if(filterType){
+//   filterType.data.push(filter.filterName)
+
+// }
+console.log(filterType)
+
+
+this.topFiltersArray.push(filter.filterName)
+console.log(this.filters)
+  }
+  
+    
+  
 
 }
--- src/app/shared/components/filter-accordion/filter-accordion.component.ts
+++ src/app/shared/components/filter-accordion/filter-accordion.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-accordion',
@@ -11,20 +11,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class FilterAccordionComponent {
 
   @Output() filterEvent = new EventEmitter<string>()
-  filters = [
-    {
-      type: "Language",
-      data: ['Hindi', 'English', 'Gujrati', 'Marathi', 'Malayalam', 'Punjabi', 'Telugu']
-    },
-    {
-      type: "Genres",
-      data: ['Drama', 'Action', 'Comedy', 'Thriller']
-    },
-    {
-      type: "Formats",
-      data: ['2D', '3D', '4Dx', 'IMAX2d']
-    },
-  ]
+@Input() filters:any =[]
   filterShowButtons: boolean = false;
   filterText: any[] = ['Hindi', 'English', 'Gujrati', 'Marathi', 'Malayalam', 'Punjabi', 'Telugu']
 
@@ -37,7 +24,7 @@ export class FilterAccordionComponent {
   }
 
 
-  applyFilter(filter: string) {
+  applyFilter(filter: any) {
     this.filterEvent.emit(filter)
 
   }
