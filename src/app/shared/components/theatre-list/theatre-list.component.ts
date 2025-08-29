f0fa8f47fd36f2f8f43a230368604ec0f0b587cc
Manu Shukla <shuklamanu246@gmail.com>
2025-08-29 14:15:23 +0530

    push all code

---
+++ src/app/shared/components/theatre-list/theatre-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theatre-list',
+  standalone: false,
+  templateUrl: './theatre-list.component.html',
+  styleUrl: './theatre-list.component.scss'
+})
+export class TheatreListComponent {
+
+}
