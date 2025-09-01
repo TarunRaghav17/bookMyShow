1daf14048a7cc35b8c0ac91eddab62637ff12e74
Manu Shukla <shuklamanu246@gmail.com>
2025-09-01 15:18:09 +0530

    comments resolve  proper syntax for subscribe

--- src/app/auth/user-auth/user-auth.component.ts
+++ src/app/auth/user-auth/user-auth.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { AuthService } from '../auth-service.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-auth',
@@ -28,7 +28,7 @@ export class UserAuthComponent implements OnInit {
   signupForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     username: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.pattern(/^[^@]+@[^@]+\.[^@]+$/)]),
     phoneNumber: new FormControl('', [
       Validators.required,
       Validators.pattern(/^[0-9]{10}$/)
@@ -37,25 +37,31 @@ export class UserAuthComponent implements OnInit {
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
 
-
   onLoginSubmit() {
     if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value).subscribe((res) => {
-        alert(res.message);
-        localStorage.setItem('token', res.data.token);
-        this.authService.userDetailsSignal.set(this.authService.decodeToken(res.data.token));
-        this.loginForm.reset();
-        this.activeModal.close(UserAuthComponent);
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res: any) => {
+          alert(res.message);
+          localStorage.setItem('token', res.data.token);
+          this.authService.userDetailsSignal.set(
+            this.authService.decodeToken(res.data.token)
+          );
+          this.loginForm.reset();
+          this.activeModal.close(UserAuthComponent);
+        },
+        error: (err) => console.error(err)
       });
     }
   }
 
-
   onSignupSubmit() {
     if (this.signupForm.valid) {
-      this.authService.signup(this.signupForm.value).subscribe(() => {
-        this.signupForm.reset();
-        this.activeModal.close(UserAuthComponent);
+      this.authService.signup(this.signupForm.value).subscribe({
+        next: () => {
+          this.signupForm.reset();
+          this.activeModal.close(UserAuthComponent);
+        },
+        error: (err) => console.error(err)
       });
     }
   }
