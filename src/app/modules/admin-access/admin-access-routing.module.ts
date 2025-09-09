dd6bf2e34b2a2c98a9844a96c69ee0f557381076
Inzamam-khan <khanloveekhan>
2025-09-09 23:10:24 +0530

    added create-venue component,setup admin access only route in routing

--- src/app/modules/admin-access/admin-access-routing.module.ts
+++ src/app/modules/admin-access/admin-access-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListYourShowComponent } from './list-your-show/list-your-show.component';
 import { UsersComponent } from './users/users.component';
+import { CreateVenueComponent } from './create-venue/create-venue.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'list-your-show', pathMatch: 'full' },
   { path: 'list-your-show', component: ListYourShowComponent },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+
+
+   /**
+  * @description route to access /admin/create/venue to create venue 
+  * @author Inzamam
+  */
+  {
+    path:'create/venue',
+    component:CreateVenueComponent
+  }
 ];
 
 @NgModule({
--- src/app/modules/admin-access/admin-access.module.ts
+++ src/app/modules/admin-access/admin-access.module.ts
@@ -5,14 +5,17 @@ import { AdminAccessRoutingModule } from './admin-access-routing.module';
 import { UsersComponent } from './users/users.component';
 import { ListYourShowComponent } from './list-your-show/list-your-show.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CreateVenueComponent } from './create-venue/create-venue.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     UsersComponent,
-    ListYourShowComponent
+    ListYourShowComponent,CreateVenueComponent
   ],
   imports: [
+    ReactiveFormsModule, CommonModule,
     CommonModule,
     AdminAccessRoutingModule, CarouselModule
   ]
