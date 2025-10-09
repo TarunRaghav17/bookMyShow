6967b8c57e563844fe5aa06a58e2f252030ed2df
Manu-01 <shuklamanu246@gmail.com>
2025-10-09 15:46:52 +0530

    Fixed Event Booking

--- src/app/shared/components/booking-events/booking-events.component.ts
+++ src/app/shared/components/booking-events/booking-events.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { CommonService } from '../../../services/common.service';
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-booking-events',
@@ -10,22 +11,25 @@ import { Location } from '@angular/common';
   templateUrl: './booking-events.component.html',
   styleUrl: './booking-events.component.scss'
 })
-export class BookingEventsComponent implements OnInit{
-  money: number = 300;
+export class BookingEventsComponent implements OnInit {
+  money!: number 
   totalMoney: number = 0;
   value: number = 1;
   add: boolean = false;
-  allShows:any[]=[]
-  title:string|null =''
-   
-constructor(private commonService:CommonService , private toastr: ToastrService ,private route:ActivatedRoute , private router:Router , private location:Location){
+  allShows: any[] = []
+  title!: string | null 
+  date!: string | null
+   bookeSeats: any[] = []
 
-}
-  
-ngOnInit(): void {
-  this.title = this.route.snapshot.paramMap.get('eventname')
-  this.getShows()
-}
+  constructor(private commonService: CommonService, private toastr: ToastrService, private route: ActivatedRoute, private router: Router, private location: Location, private modalService: NgbModal) {
+
+  }
+
+  ngOnInit(): void {
+    this.title = this.route.snapshot.paramMap.get('eventname')
+    this.date = this.route.snapshot.paramMap.get('date')
+    this.getShows()
+  }
 
   addMember() {
     this.add = true;
@@ -50,25 +54,52 @@ ngOnInit(): void {
     this.totalMoney = this.value * this.money;
   }
 
-
-getShows(){
-  let eventId:string| null= this.route.snapshot.paramMap.get('id')
-   let date:string| null= this.route.snapshot.paramMap.get('date')
-    this.commonService.getShowsById(eventId,date).subscribe({
-      next:(res)=>{
+  getShows() {
+    let eventId: string | null = this.route.snapshot.paramMap.get('id')
+    let date: string | null = this.route.snapshot.paramMap.get('date')
+    this.commonService.getShowsById(eventId, date).subscribe({
+      next: (res) => {
         this.allShows = res.data
+        this.money = this.allShows[0]?.shows[0]?.availableCategories[0]?.categoryPrice
       },
-      error:(err)=>{
+      error: (err) => {
         this.toastr.error(err.message)
       }
     })
   }
 
-   onBooking(){
-    this.toastr.success("Ticket booked successfully")
-    this.router.navigate(['/explore/home'])
+  goBack() {
+    this.location.back()
   }
-   goBack(){
-  this.location.back()
+
+  bookingConfirmation(confirmModal: TemplateRef<any>) {
+    for (let i = 0; i < this.value; i++) {
+      this.generateRandomCode()
+    }
+    const modalRef = this.modalService.open(confirmModal, {
+      ariaLabelledBy: 'modal-basic-title',
+      modalDialogClass: 'no-border-modal',
+      backdrop: 'static'
+    });
+    modalRef.result.then((result) => {
+      if (result === 'Book') {
+        this.toastr.success(`your ticekt for ${this.title} booked successfully `)
+      }
+      setTimeout(() => {
+        this.router.navigate(['/explore/home'])
+      }, 300)
+
+    })
   }
+
+  alphabets = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+  ]
+  generateRandomCode() {
+    const randomLetter = this.alphabets[Math.floor(Math.random() * this.alphabets.length)]
+    const randomNumber = String(Math.floor(Math.random() * 100)).padStart(2, '0');
+    this.bookeSeats.push(`${randomLetter}-${randomNumber}`);
+  }
+  
 }
